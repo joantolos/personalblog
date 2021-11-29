dc580f97a0e2981639ec8d3db73263bd0c20dd86
joantolos <joan.tolos@gmail.com>
2021-11-29 12:33:32 +0100

    Disabling scroll for hamburguer menu when opened

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,23 +1,40 @@
 function openHamburguerMenu() {
   var x = document.getElementById("links");
   if (x.style.display === "block") {
-    x.style.display = "none";
+    closeMenu(x);
   } else {
-    x.style.display = "block";
+    openMenu(x);
   }
 
   var x = document.getElementById("hamburger-icon");
   if (x.style.display === "block") {
-    x.style.display = "none";
+    closeMenu(x);
   } else {
-    x.style.display = "block";
+    openMenu(x);
   }
 
   var x = document.getElementById("hamburger-close");
   if (x.style.display === "block") {
-    x.style.display = "none";
+    closeMenu(x);
   } else {
-    x.style.display = "block";
+    openMenu(x);
   }
 
+}
+
+function closeMenu(x) {
+  x.style.display = "none";
+  window.onscroll = function () { };
+}
+
+function openMenu(x) {
+  x.style.display = "block";
+  // Get the current page scroll position
+  scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+
+    // if any scroll is attempted, set this to the previous value
+    window.onscroll = function () {
+      window.scrollTo(scrollLeft, scrollTop);
+    };
 }
